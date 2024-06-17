4cc3cfac65c084a8ca6d57019f86594293072261
Noe Garcia Lara <noegarcialara02@gmail.com>
2024-06-16 21:11:35 -0600

    Agrega APIRest y conexion, ademas toma datos del formulario

---
+++ js/conexionAPI.js
@@ -0,0 +1,30 @@
+async function listarProductos(){
+    const conexion = await fetch("http://localhost:3000/productos");
+
+    const conexionConvertida = conexion.json();
+
+    return conexionConvertida;
+}
+
+async function enviarProducto(nombre, precio, urlImagen) {
+    const conexion = await fetch ("http://localhost:3000/productos",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({
+            nombre:nombre,
+            precio:`$${precio}.00`,
+            urlImagen:urlImagen
+        })
+    })
+    const conexionConvertida = conexion.json();
+
+    if(!conexion.ok){
+        throw new Error ("Ha ocurrido un error al enviar el video");
+    }
+
+    return conexionConvertida;
+}
+
+export const conexionAPI = {
+    listarProductos, enviarProducto
+}
---
+++ js/crearProducto.js
@@ -0,0 +1,20 @@
+import { conexionAPI } from "./conexionAPI";
+
+const formulario = document.querySelector("[data-formulario]");
+
+async function crearProducto(evento){
+
+    evento.preventDefault();
+
+    const nombre = document.querySelector("[data-nombre]").value;
+    const precio = document.querySelector("[data-precio]").value;
+    const urlImagen = document.querySelector("[data-urlImagen]").value;
+
+    try {
+        await conexionAPI.enviarProducto(nombre, precio, urlImagen);
+    } catch (error) {
+        alert(error)
+    }
+}
+
+formulario.addEventListener("submit", evento => crearProducto(evento));
